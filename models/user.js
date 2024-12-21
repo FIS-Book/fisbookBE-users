a66afb41528414d32a3c977b11d00434e0d0bf77
Yasmina Moreira Bawazeer <yasmorbaw@alum.us.es>
2024-12-21 16:36:22 +0000

    FEAT: adding information to user.js

--- models/user.js
+++ models/user.js
@@ -5,27 +5,44 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: true
     },
+    apellidos: {
+        type: String,
+        required: true
+    },
     email: {
         type: String,
         required: true
     },
     plan: {
         type: String,
+        enum: ['Activo', 'No activo'],
         required: true
     },
     tipo: {
         type: String,
+        enum: ['Admin', 'User'], // Define el tipo de usuario
         required: true
     },
+    listaLecturasId: [{ type: String }] // Opcional: IDs de listas gestionadas por otro microservicio
+    ,
+    numDescargas: {
+        type: Number,
+        default: 0 // Número de descargas inicial
+    },
+    resenasId: [{ type: String}] // Referencia a reseñas que haya escrito
 });
 
 userSchema.methods.cleanup = function() {
     return {
         id: this._id,
         nombre: this.nombre,
+        apellidos: this.apellidos,
         email: this.email,
         plan: this.plan,
-        tipo: this.tipo
+        tipo: this.tipo,
+        listaLecturasId: this.listaLecturasId,
+        numDescargas: this.numDescargas,
+        resenasId: this.resenasId
     }
 }
 
