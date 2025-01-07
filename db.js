a5a882a1b680893f2148cd7a54e89a2f4074c858
krilaclop <krilaclop@alum.us.es>
2025-01-08 00:00:37 +0100

    FIX: Pruebas

--- db.js
+++ db.js
@@ -1,18 +1,16 @@
+// db.js
 const mongoose = require('mongoose');
-require('dotenv').config();
- 
-//Function to connect to the database
+
 async function connectToDatabase() {
     try {
-        const DB_URL = process.env.NODE_ENV === 'test' ? process.env.MONGO_URI_USERS_TEST : process.env.MONGO_URI_USERS;
-        //console.log('DB_URL:', DB_URL)
-        await mongoose.connect(DB_URL);
+        await mongoose.connect(process.env.MONGO_URI_USERS_TEST, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
         console.log('Connected to MongoDB successfully');
     } catch (error) {
         console.error('Error connecting to MongoDB:', error.message);
     }
 }
-connectToDatabase();
-// Access the Mongoose connection instance
-const db = mongoose.connection;
-module.exports = db;
+
+module.exports = connectToDatabase;
